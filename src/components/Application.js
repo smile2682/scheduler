8ac2e7d1b04e80ce7aac457270744ad7ac71e720
smile2682 <smilecrystal.liyi@gmail.com>
2021-09-03 17:14:26 -0700

    build helper func,test and axio to retrive interviewer Data.

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import DayList from './DayList'
 import "components/Application.scss";
 import Appointment from "components/Appointment/index";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 import axios from "axios";
 
 
@@ -14,35 +14,33 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
- 
   const setDay = day => setState({ ...state, day });
-  // const setDays = (days) => setState(prev => ({ ...prev, days }));
+  // need to wrap with useEffect, nor the get api and set state will,
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ])
+      .then((all) => {
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      })
+  }, []);
+  // console.log("after",state.interviewers)
 
+  const appointments = getAppointmentsForDay(state, state.day);
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
 
-
-  // use axios to fetch data
-  // useEffect(() => {
-  //   const DaysApi = 'http://localhost:8001/api/days';
-  //   axios.get(DaysApi)
-  // //     .then(response => setDays([...response.data]));
-  // // }, [])
-
-  Promise.all([
-    axios.get("http://localhost:8001/api/days"),
-    axios.get("http://localhost:8001/api/appointments"),
-  ])
-  .then((all) => {
-    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
   });
-  
-    
-
-  const Parsedappointment = dailyAppointments.map(
-    appointment => {
-      return <Appointment key={appointment.id}{...appointment} />
-    }
-  )
   return (
     <main className="layout">
       <section className="sidebar">
@@ -68,7 +66,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {Parsedappointment}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,23 @@
-export function getAppointmentsForDay(state, day) {
-  const AppoArray=[];
-  for (const Day of state.days){
-  if(Day.name === day){
-    Day.appointments.forEach(number=>AppoArray.push(state.appointments[number]))
+function getAppointmentsForDay(state, day) {
+  const AppoArray = [];
+  for (const Day of state.days) {
+    if (Day.name === day) {
+      Day.appointments.forEach(number => AppoArray.push(state.appointments[number]))
+    }
   }
-}
   //... returns an array of appointments for that day
   return AppoArray;
 }
+
+function getInterview(state, interview) {
+  if (interview) {
+    const name =state.interviewers[interview.interviewer] 
+    return {
+      student: interview.student,
+      interviewer: name
+    }
+  }
+  return null
+}
+
+export { getAppointmentsForDay, getInterview };
