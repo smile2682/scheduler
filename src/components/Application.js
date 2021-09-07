845eeb616cc8d348a2c7ad887ed55f04ca4301d4
smile2682 <smilecrystal.liyi@gmail.com>
2021-09-07 11:15:50 -0700

    add useApplicationData hook to modualarize.

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,80 +4,23 @@ import "components/Application.scss";
 import Appointment from "components/Appointment/index";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import axios from "axios";
+import useApplicationData from "hooks/useApplicationData";
 
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {}
-  });
 
-  const setDay = day => setState({ ...state, day });
-  // need to wrap with useEffect, nor the get api and set state will,
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
-    ])
-      .then((all) => {
-        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      })
-  }, []);
-  // console.log("after",state.interviewers)
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+
 
   const appointments = getAppointmentsForDay(state, state.day);
   const schedule = appointments.map((appointment) => {
   const interview = getInterview(state, appointment.interview);
   const interviewersForDay = getInterviewersForDay(state, state.day)
-
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios
-      .put(`/api/appointments/${id}`, {
-        // shall  I spread the id and time? how does this put work?
-        interview,
-      })
-      .then(result => {
-        setState({
-          ...state,
-          appointments
-        });
-        console.log(result)
-      });
-    
-  };
-  
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    return axios.delete(`/api/appointments/${id}`, {}).then((result) => {
-      console.log(result)
-      setState({
-        ...state,
-        appointments,
-      });
-    });
-  }
-
     return (
       <Appointment
         key={appointment.id}
@@ -90,6 +33,8 @@ export default function Application(props) {
       />
     );
   });
+
+
   return (
     <main className="layout">
       <section className="sidebar">
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+
+
+const [state, setState] = useState({
+  day: "Monday",
+  days: [],
+  // you may put the line below, but will have to remove/comment hardcoded appointments variable
+  appointments: {}
+});
+
+
+
+const setDay = day => setState({ ...state, day });
+// need to wrap with useEffect, nor the get api and set state will,
+useEffect(() => {
+  Promise.all([
+    axios.get("http://localhost:8001/api/days"),
+    axios.get("http://localhost:8001/api/appointments"),
+    axios.get("http://localhost:8001/api/interviewers")
+  ])
+    .then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    })
+}, []);
+// console.log("after",state.interviewers)
+
+
+function bookInterview(id, interview) {
+  console.log(id, interview);
+  const appointment = {
+    ...state.appointments[id],
+    interview: { ...interview }
+  };
+  const appointments = {
+    ...state.appointments,
+    [id]: appointment
+  };
+
+  return axios
+    .put(`/api/appointments/${id}`, {
+      // shall  I spread the id and time? how does this put work?
+      interview,
+    })
+    .then(result => {
+      setState({
+        ...state,
+        appointments
+      });
+      console.log(result)
+    });
+  
+};
+
+function cancelInterview(id) {
+  const appointment = {
+    ...state.appointments[id],
+    interview: null,
+  };
+  const appointments = {
+    ...state.appointments,
+    [id]: appointment,
+  };
+
+  return axios.delete(`/api/appointments/${id}`, {}).then((result) => {
+    console.log(result)
+    setState({
+      ...state,
+      appointments,
+    });
+  });
+}
+  return {state, setDay, bookInterview, cancelInterview}
+}
